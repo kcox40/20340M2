caadcc91b9a987c6fc95ff4842af2fe23babc9a7
Unknown <abigail_lacy14@gatech.edu>
2017-09-08 11:39:14 -0400

    Updates p2 to return a dummy string

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -35,7 +35,7 @@ public class Person2 {
 	  //take indices and scramble them randomly
 	  //put letters back at scrambled indices
 	  //return something
-	  return null;
+	  return "word";
 	}
 	/**
 	 * Return a string rep of this object
